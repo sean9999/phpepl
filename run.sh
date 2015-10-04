baad6500adc0e46b6effc579d57841052114beb7
Sean Macdonald <sean@crazyhorsecoding.com>
2015-10-04 11:02:25 -0400

    automatically launch browser on run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@ source $DIR/vars.sh
 docker stop "$containername" 2> /dev/null
 docker rm "$containername" 2> /dev/null
 
-docker run -P --rm \
+docker run -P -d \
 	-v $DIR/code:/var/www/public/ \
 	-v $DIR/logz:/var/log/apache2/ \
 	--name "$containername" \
     "$imagename"
+
+bash $DIR/browse.sh
